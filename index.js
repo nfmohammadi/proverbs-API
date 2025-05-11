c17c2f1e68557168fdcab657bfcaae2c0115e2bc
Nmohammadi <53583653+nfmohammadi@users.noreply.github.com>
2025-05-11 13:59:42 +0430

    changes made in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import fs from 'fs';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
